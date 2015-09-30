e9c54de0ddfa28e67a292833528c202e5ba9bc04
Tomas Popela <tpopela@redhat.com>
2015-09-30 08:52:12 +0200

    Rebased Guido Günther's work from [0] on top of 2.52.0
    [0] - http://anonscm.debian.org/cgit/users/agx/libsoup.git/log/

--- libsoup/soup-auth.h
+++ libsoup/soup-auth.h
@@ -115,6 +115,8 @@ GType soup_auth_digest_get_type (void);
 #define SOUP_TYPE_AUTH_NTLM   (soup_auth_ntlm_get_type ())
 SOUP_AVAILABLE_IN_2_4
 GType soup_auth_ntlm_get_type   (void);
+#define SOUP_TYPE_AUTH_NEGOTIATE  (soup_auth_negotiate_get_type ())
+GType soup_auth_negotiate_get_type   (void);
 
 /* Deprecated SoupPasswordManager-related APIs: all are now no-ops */
 SOUP_AVAILABLE_IN_2_28
