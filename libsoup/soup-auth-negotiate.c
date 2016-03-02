0d662aedfeca364657137865d63745d7480bfeb1
Tomas Popela <tpopela@redhat.com>
2016-03-02 15:09:38 +0100

    soup-auth-negotiate: Reorganize the sources to get rid of some ifdefs

--- libsoup/soup-auth-negotiate.c
+++ libsoup/soup-auth-negotiate.c
@@ -24,11 +24,10 @@
 #include "soup-misc-private.h"
 #include "soup-uri.h"
 
-#ifdef LIBSOUP_HAVE_GSSAPI
-const gboolean soup_auth_negotiate_supported = TRUE;
-#else
+#ifndef LIBSOUP_HAVE_GSSAPI
 const gboolean soup_auth_negotiate_supported = FALSE;
-#endif /* LIBSOUP_HAVE_GSSAPI */
+#else
+const gboolean soup_auth_negotiate_supported = TRUE;
 
 G_DEFINE_TYPE (SoupAuthNegotiate, soup_auth_negotiate, SOUP_TYPE_CONNECTION_AUTH)
 
@@ -47,10 +46,8 @@ typedef struct {
 	gboolean initialized;
 	gchar *response_header;
 
-#ifdef LIBSOUP_HAVE_GSSAPI
 	gss_ctx_id_t context;
 	gss_name_t   server_name;
-#endif /* LIBSOUP_HAVE_GSSAPI */
 
 	SoupNegotiateState state;
 } SoupNegotiateConnectionState;
@@ -66,7 +63,6 @@ typedef struct {
 
 #define SOUP_AUTH_NEGOTIATE_GET_PRIVATE(o) (G_TYPE_INSTANCE_GET_PRIVATE ((o), SOUP_TYPE_AUTH_NEGOTIATE, SoupAuthNegotiatePrivate))
 
-#ifdef LIBSOUP_HAVE_GSSAPI
 static gboolean check_auth_trusted_uri (SoupConnectionAuth *auth,
 					SoupMessage *msg);
 static gboolean soup_gss_build_response (SoupNegotiateConnectionState *conn,
@@ -87,13 +83,6 @@ static void remove_server_response_handler (SoupMessage *msg, gpointer auth);
 
 static const char spnego_OID[] = "\x2b\x06\x01\x05\x05\x02";
 static const gss_OID_desc gss_mech_spnego = { sizeof (spnego_OID) - 1, (void *) &spnego_OID };
-#endif /* LIBSOUP_HAVE_GSSAPI */
-
-static void
-soup_auth_negotiate_init (SoupAuthNegotiate *negotiate)
-{
-	g_object_set (G_OBJECT (negotiate), SOUP_AUTH_REALM, "", NULL);
-}
 
 static gpointer
 soup_auth_negotiate_create_connection_state (SoupConnectionAuth *auth)
@@ -111,9 +100,7 @@ soup_auth_negotiate_create_connection_state (SoupConnectionAuth *auth)
 static void
 free_connection_state_data (SoupNegotiateConnectionState *conn)
 {
-#ifdef LIBSOUP_HAVE_GSSAPI
 	soup_gss_client_cleanup (conn);
-#endif /* LIBSOUP_HAVE_GSSAPI */
 	g_free (conn->response_header);
 }
 
@@ -129,74 +116,6 @@ soup_auth_negotiate_free_connection_state (SoupConnectionAuth *auth,
 
 	g_slice_free (SoupNegotiateConnectionState, conn);
 	priv->conn_state = NULL;
-}
-
-static gboolean
-soup_auth_negotiate_update_connection (SoupConnectionAuth *auth, SoupMessage *msg,
-				       const char *header, gpointer state)
-{
-#ifdef LIBSOUP_HAVE_GSSAPI
-	gboolean success = TRUE;
-	SoupNegotiateConnectionState *conn = state;
-	SoupAuthNegotiatePrivate *priv = SOUP_AUTH_NEGOTIATE_GET_PRIVATE (auth);
-	GError *err = NULL;
-
-	if (!check_auth_trusted_uri (auth, msg)) {
-		conn->state = SOUP_NEGOTIATE_FAILED;
-		goto out;
-	}
-
-	/* Found negotiate header with no token, start negotiate */
-	if (strcmp (header, "Negotiate") == 0) {
-		/* If we were already negotiating and we get a 401
-		 * with no token, start again. */
-		if (conn->state == SOUP_NEGOTIATE_SENT_RESPONSE) {
-			free_connection_state_data (conn);
-			conn->initialized = FALSE;
-		}
-
-		conn->state = SOUP_NEGOTIATE_RECEIVED_CHALLENGE;
-		if (soup_gss_build_response (conn, SOUP_AUTH (auth), &err)) {
-			/* Register the callbacks just once */
-			if (priv->message_finished_signal_id == 0) {
-				gulong id = 0;
-				id = g_signal_connect (msg,
-						       "finished",
-						       G_CALLBACK (remove_server_response_handler),
-						       auth);
-				priv->message_finished_signal_id = id;
-			}
-
-			if (priv->message_got_headers_signal_id == 0) {
-				gulong id = 0;
-				/* Wait for the 2xx response to verify server response */
-				id = g_signal_connect (msg,
-						       "got_headers",
-						       G_CALLBACK (check_server_response),
-						       auth);
-				priv->message_got_headers_signal_id = id;
-			}
-			goto out;
-		} else {
-			/* FIXME: report further upward via
-			 * soup_message_get_error_message  */
-			g_warning ("gssapi step failed: %s", err->message);
-			success = FALSE;
-		}
-	} else if (!strncmp (header, "Negotiate ", 10)) {
-		if (soup_gss_client_step (conn, header + 10, &err) == AUTH_GSS_CONTINUE) {
-			conn->state = SOUP_NEGOTIATE_RECEIVED_CHALLENGE;
-			goto out;
-		}
-	}
-
-	conn->state = SOUP_NEGOTIATE_FAILED;
- out:
-	g_clear_error (&err);
-	return success;
-#else
-	return FALSE;
-#endif /* LIBSOUP_HAVE_GSSAPI */
 }
 
 static GSList *
@@ -269,6 +188,81 @@ soup_auth_negotiate_is_connection_ready (SoupConnectionAuth *auth,
 
 	return conn->state != SOUP_NEGOTIATE_FAILED;
 }
+#endif /* LIBSOUP_HAVE_GSSAPI */
+
+static gboolean
+soup_auth_negotiate_update_connection (SoupConnectionAuth *auth, SoupMessage *msg,
+				       const char *header, gpointer state)
+{
+#ifdef LIBSOUP_HAVE_GSSAPI
+	gboolean success = TRUE;
+	SoupNegotiateConnectionState *conn = state;
+	SoupAuthNegotiatePrivate *priv = SOUP_AUTH_NEGOTIATE_GET_PRIVATE (auth);
+	GError *err = NULL;
+
+	if (!check_auth_trusted_uri (auth, msg)) {
+		conn->state = SOUP_NEGOTIATE_FAILED;
+		goto out;
+	}
+
+	/* Found negotiate header with no token, start negotiate */
+	if (strcmp (header, "Negotiate") == 0) {
+		/* If we were already negotiating and we get a 401
+		 * with no token, start again. */
+		if (conn->state == SOUP_NEGOTIATE_SENT_RESPONSE) {
+			free_connection_state_data (conn);
+			conn->initialized = FALSE;
+		}
+
+		conn->state = SOUP_NEGOTIATE_RECEIVED_CHALLENGE;
+		if (soup_gss_build_response (conn, SOUP_AUTH (auth), &err)) {
+			/* Register the callbacks just once */
+			if (priv->message_finished_signal_id == 0) {
+				gulong id = 0;
+				id = g_signal_connect (msg,
+						       "finished",
+						       G_CALLBACK (remove_server_response_handler),
+						       auth);
+				priv->message_finished_signal_id = id;
+			}
+
+			if (priv->message_got_headers_signal_id == 0) {
+				gulong id = 0;
+				/* Wait for the 2xx response to verify server response */
+				id = g_signal_connect (msg,
+						       "got_headers",
+						       G_CALLBACK (check_server_response),
+						       auth);
+				priv->message_got_headers_signal_id = id;
+			}
+			goto out;
+		} else {
+			/* FIXME: report further upward via
+			 * soup_message_get_error_message  */
+			g_warning ("gssapi step failed: %s", err->message);
+			success = FALSE;
+		}
+	} else if (!strncmp (header, "Negotiate ", 10)) {
+		if (soup_gss_client_step (conn, header + 10, &err) == AUTH_GSS_CONTINUE) {
+			conn->state = SOUP_NEGOTIATE_RECEIVED_CHALLENGE;
+			goto out;
+		}
+	}
+
+	conn->state = SOUP_NEGOTIATE_FAILED;
+ out:
+	g_clear_error (&err);
+	return success;
+#else
+	return FALSE;
+#endif /* LIBSOUP_HAVE_GSSAPI */
+}
+
+static void
+soup_auth_negotiate_init (SoupAuthNegotiate *negotiate)
+{
+	g_object_set (G_OBJECT (negotiate), SOUP_AUTH_REALM, "", NULL);
+}
 
 static void
 soup_auth_negotiate_class_init (SoupAuthNegotiateClass *auth_negotiate_class)
@@ -277,23 +271,23 @@ soup_auth_negotiate_class_init (SoupAuthNegotiateClass *auth_negotiate_class)
 	SoupConnectionAuthClass *conn_auth_class =
 			SOUP_CONNECTION_AUTH_CLASS (auth_negotiate_class);
 
-	g_type_class_add_private (auth_negotiate_class, sizeof (SoupAuthNegotiatePrivate));
-
 	auth_class->scheme_name = "Negotiate";
 	auth_class->strength = 7;
+
+	conn_auth_class->update_connection = soup_auth_negotiate_update_connection;
+#ifdef LIBSOUP_HAVE_GSSAPI
+	conn_auth_class->create_connection_state = soup_auth_negotiate_create_connection_state;
+	conn_auth_class->free_connection_state = soup_auth_negotiate_free_connection_state;
+	conn_auth_class->get_connection_authorization = soup_auth_negotiate_get_connection_authorization;
+	conn_auth_class->is_connection_ready = soup_auth_negotiate_is_connection_ready;
 
 	auth_class->get_protection_space = soup_auth_negotiate_get_protection_space;
 	auth_class->authenticate = soup_auth_negotiate_authenticate;
 	auth_class->is_authenticated = soup_auth_negotiate_is_authenticated;
 	auth_class->can_authenticate = soup_auth_negotiate_can_authenticate;
 
-	conn_auth_class->create_connection_state = soup_auth_negotiate_create_connection_state;
-	conn_auth_class->free_connection_state = soup_auth_negotiate_free_connection_state;
-	conn_auth_class->update_connection = soup_auth_negotiate_update_connection;
-	conn_auth_class->get_connection_authorization = soup_auth_negotiate_get_connection_authorization;
-	conn_auth_class->is_connection_ready = soup_auth_negotiate_is_connection_ready;
+	g_type_class_add_private (auth_negotiate_class, sizeof (SoupAuthNegotiatePrivate));
 
-#ifdef LIBSOUP_HAVE_GSSAPI
 	parse_uris_from_env_variable ("SOUP_GSSAPI_TRUSTED_URIS", &trusted_uris);
 	parse_uris_from_env_variable ("SOUP_GSSAPI_BLACKLISTED_URIS", &blacklisted_uris);
 #endif /* LIBSOUP_HAVE_GSSAPI */
